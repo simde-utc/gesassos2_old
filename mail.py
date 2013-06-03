7ea82c1a292cbcefcf5f3dd1479c5917e5688e60
cmercier <clemmercier@hotmail.com>
2013-06-03 18:31:08 +0200

    Maj mail et sql

--- mail.py
+++ mail.py
@@ -4,9 +4,22 @@ from fabric.api import *
 @roles('mail')
 def main():
   print("mail.main")
+  
+def gen_mdp(length):
+	print("le generateur")
+	# 26 lettres majuscules + 26 lettres minuscules + 10 chiffres [0..9] = 62
+	letters = string.printable[:62]
+	password = ''
+	for ununsed_count in range(length):
+		password += letters[random.randint(0, len(letters) - 1)]
 
 def add_mail(login):
-  print("creation dossiers")
+  sudo('mkdir /mails/%s' % login)
+  sudo('chown -R %s:"Domain Users" /mails/%s' % (login, login))
+  sudo('chmod 0700 -R /mails/%s' % login)
   
 def add_assotous(login):
   print("ajout dans la bdd mailman")
+  print("connexion a la bdd mailman")
+  mdp = gen_mdp(8)
+  sudo(INSERT INTO mailman_mysql (listname, address, hide, nomail, ack, not_metoo, digest, plain, password, lang, name, one_last_digest, user_options, delivery_status, topics_userinterest, delivery_status_timestamp, bi_cookie, bi_score, bi_noticesleft, bi_lastnotice, bi_date) VALUES (‘asso-tous’, ‘$login@assos.utc.fr’, 'N', 'N', 'Y', 'Y', 'N', 'N', %s, 'fr', '', 'N', '264', '0', NULL, '0000-00-00 00:00:00', NULL, '0', '0', '0000-00-00', '0000-00-00') % mdp)
--- sql.py
+++ sql.py
@@ -1,9 +1,23 @@
 from fabric.api import *
+import random
+import string
 
 @task(default=True)
 @roles('sql')
 def main():
   print("sql.main")
+  
+def gen_mdp(length):
+	print("le generateur")
+	# 26 lettres majuscules + 26 lettres minuscules + 10 chiffres [0..9] = 62
+	letters = string.printable[:62]
+	password = ''
+	for ununsed_count in range(length):
+		password += letters[random.randint(0, len(letters) - 1)]
 
 def add_sql(login, password):
   print("generation mot de passse et appel de la procedure")
+  mdp = gen_mdp(8)
+  print("connexion a la bdd gesassos")
+  sudo("CALL createUser('%s','%s')" % (login, mdp))
+  sudo("CREATE DATABASE %s" % login)
