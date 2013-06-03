57f04a86fb4e461f2961b4edcc1d0a5e2182aeea
cmercier <clemmercier@hotmail.com>
2013-06-03 23:46:37 +0200

    add connection database

--- mail.py
+++ mail.py
@@ -12,14 +12,16 @@ def gen_mdp(length):
 	password = ''
 	for ununsed_count in range(length):
 		password += letters[random.randint(0, len(letters) - 1)]
+	return password
 
 def add_mail(login):
   sudo('mkdir /mails/%s' % login)
   sudo('chown -R %s:"Domain Users" /mails/%s' % (login, login))
   sudo('chmod 0700 -R /mails/%s' % login)
   
-def add_assotous(login):
-  print("ajout dans la bdd mailman")
+def add_assotous(login):  
   print("connexion a la bdd mailman")
+  sudo("mysql -u gesassos -p %s --execute='CONNECT gesassos'" % (password))
+  print("ajout dans la bdd mailman")
   mdp = gen_mdp(8)
-  sudo(INSERT INTO mailman_mysql (listname, address, hide, nomail, ack, not_metoo, digest, plain, password, lang, name, one_last_digest, user_options, delivery_status, topics_userinterest, delivery_status_timestamp, bi_cookie, bi_score, bi_noticesleft, bi_lastnotice, bi_date) VALUES (‘asso-tous’, ‘$login@assos.utc.fr’, 'N', 'N', 'Y', 'Y', 'N', 'N', %s, 'fr', '', 'N', '264', '0', NULL, '0000-00-00 00:00:00', NULL, '0', '0', '0000-00-00', '0000-00-00') % mdp)
+  sudo("mysql -u gesassos -p %s --execute='INSERT INTO mailman_mysql (listname, address, hide, nomail, ack, not_metoo, digest, plain, password, lang, name, one_last_digest, user_options, delivery_status, topics_userinterest, delivery_status_timestamp, bi_cookie, bi_score, bi_noticesleft, bi_lastnotice, bi_date) VALUES (‘asso-tous’, ‘$login@assos.utc.fr’, 'N', 'N', 'Y', 'Y', 'N', 'N', %s, 'fr', '', 'N', '264', '0', NULL, '0000-00-00 00:00:00', NULL, '0', '0', '0000-00-00', '0000-00-00')'" % (password, mdp))
--- sql.py
+++ sql.py
@@ -14,10 +14,12 @@ def gen_mdp(length):
 	password = ''
 	for ununsed_count in range(length):
 		password += letters[random.randint(0, len(letters) - 1)]
+	return password
 
 def add_sql(login, password):
   print("generation mot de passse et appel de la procedure")
   mdp = gen_mdp(8)
   print("connexion a la bdd gesassos")
-  sudo("CALL createUser('%s','%s')" % (login, mdp))
-  sudo("CREATE DATABASE %s" % login)
+  sudo("mysql -u gesassos -p %s --execute='CONNECT gesassos'" % (password))
+  sudo("mysql -u gesassos -p %s --execute='CALL createUser('%s','%s');' " % (password, login, mdp))
+  sudo("mysql -u %s -p %s --execute='CREATE DATABASE %s;' " % (login, mdp, login))
