858d3769b476e773f8d5f245b3216b4bc501ced5
cmercier <clemmercier@hotmail.com>
2013-06-05 15:19:22 +0200

    modif mysqldb

--- mail.py
+++ mail.py
@@ -1,4 +1,8 @@
 from fabric.api import *
+import random
+import string
+import MySQLdb
+
 
 @task(default=True)
 @roles('mail')
@@ -21,7 +25,11 @@ def add_mail(login):
   
 def add_assotous(login):  
   print("connexion a la bdd mailman")
-  sudo("mysql -u gesassos -p %s --execute='CONNECT gesassos'" % (password))
-  print("ajout dans la bdd mailman")
   mdp = gen_mdp(8)
-  sudo("mysql -u gesassos -p %s --execute='INSERT INTO mailman_mysql (listname, address, hide, nomail, ack, not_metoo, digest, plain, password, lang, name, one_last_digest, user_options, delivery_status, topics_userinterest, delivery_status_timestamp, bi_cookie, bi_score, bi_noticesleft, bi_lastnotice, bi_date) VALUES (‘asso-tous’, ‘$login@assos.utc.fr’, 'N', 'N', 'Y', 'Y', 'N', 'N', %s, 'fr', '', 'N', '264', '0', NULL, '0000-00-00 00:00:00', NULL, '0', '0', '0000-00-00', '0000-00-00')'" % (password, mdp))
+  db=MySQLdb.connect(user="gesassos", passwd="",db="mailman")
+  c=db.cursor()
+  print("ajout dans la bdd mailman")
+  c.execute("INSERT INTO mailman_mysql (listname, address, hide, nomail, ack, not_metoo, digest, plain, password, lang, name, one_last_digest, user_options, delivery_status, topics_userinterest, delivery_status_timestamp, bi_cookie, bi_score, bi_noticesleft, bi_lastnotice, bi_date) VALUES (‘asso-tous’, ‘$login@assos.utc.fr’, 'N', 'N', 'Y', 'Y', 'N', 'N', %s, 'fr', '', 'N', '264', '0', NULL, '0000-00-00 00:00:00', NULL, '0', '0', '0000-00-00', '0000-00-00')" % mdp)
+  c.close()
+  db.close()
+  
--- sql.py
+++ sql.py
@@ -1,11 +1,12 @@
 from fabric.api import *
 import random
 import string
+import MySQLdb
 
 @task(default=True)
 @roles('sql')
 def main():
-  print("sql.main")
+  print("sql.main") 
   
 def gen_mdp(length):
 	print("le generateur")
@@ -20,6 +21,13 @@ def add_sql(login, password):
   print("generation mot de passse et appel de la procedure")
   mdp = gen_mdp(8)
   print("connexion a la bdd gesassos")
-  sudo("mysql -u gesassos -p %s --execute='CONNECT gesassos'" % (password))
-  sudo("mysql -u gesassos -p %s --execute='CALL createUser('%s','%s');' " % (password, login, mdp))
-  sudo("mysql -u %s -p %s --execute='CREATE DATABASE %s;' " % (login, mdp, login))
+  db=MySQLdb.connect(user="gesassos", passwd="",db="gesassos")
+  c=db.cursor()
+  c.execute("CALL createUser('%s','%s')" % (login, mdp))
+  c.close()
+  db.close()
+  db=MySQLdb.connect(user=login, passwd=mdp)
+  c=db.cursor()
+  c.execute("CREATE DATABASE %s " % login)
+  c.close()
+  db.close()
