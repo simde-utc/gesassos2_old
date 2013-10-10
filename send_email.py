3d471eeec885abbe4744f07b79c250969858c5e9
cmercier <clemmercier@hotmail.com>
2013-10-11 01:25:02 +0200

    error variable send_mail

--- send_email.py
+++ send_email.py
@@ -5,7 +5,7 @@ import smtplib
 def send_email(from, receiver, message):	
 	try:
 	   serveur = smtplib.SMTP('localhost')
-	   serveur.sendmail(from, to, message)         
+	   serveur.sendmail(from, receiver, message)         
 	   print "Successfully sent email"
 	except SMTPException:
 	   print "Error: unable to send email"
