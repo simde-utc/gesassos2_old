d83e655b68b973f4af53a1fef008b311b01b48bf
root dev <root@dev>
2013-10-11 02:11:02 +0200

    bug send_email

--- send_email.py
+++ send_email.py
@@ -2,10 +2,10 @@ from fabric.api import *
 
 import smtplib
 
-def send_email(from, receiver, message):	
+def send_email(sender, receiver, message):	
 	try:
 	   serveur = smtplib.SMTP('localhost')
-	   serveur.sendmail(from, receiver, message)         
-	   print "Successfully sent email"
+	   serveur.sendmail(sender, receiver, message)         
+	   print ("Successfully sent email")
 	except SMTPException:
-	   print "Error: unable to send email"
+	   print ("Error: unable to send email")
