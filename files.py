4136e9c2692c02bdfc8ad77876c110a3c096d4e9
cmercier <clemmercier@hotmail.com>
2013-10-28 00:12:01 +0100

    change files (smbpasswd and chmod index.php)

--- files.py
+++ files.py
@@ -10,7 +10,8 @@ def add_user(login, mdp1):
   env.host_string = 'files.mde.utc'
   sudo('smbldap-useradd -a -d /assos/%s -A1 %s' % (login, login))  
   print("Suggested password: %s" % mdp1)
-  sudo('smbpasswd %s' % login)
+  #sudo('smbpasswd %s' % login)
+  sudo('echo -ne "%s\n%s\n" | smbpasswd -s %S' % (mdp1, mdp1, login))
   sudo('mkdir -p /assos/%s' % login)
   sudo('mkdir -p /sites/%s' % login)
   sudo('mkdir -p /sites/sessions/%s' % login)
@@ -21,7 +22,8 @@ def add_user(login, mdp1):
   sudo('chmod 2750 -R /sites/%s' % login)
   sudo('chmod 2750 -R /sites/sessions/%s' % login)
   sudo("echo \"<?php\nheader('Location: http://assos.utc.fr/asso/%s');\n?>\" > /sites/%s/index.php" % (login, login))
-  sudo('chown %s /sites/%s/index.php' % (login, login))
+  sudo('chown %s:web /sites/%s/index.php' % (login, login))
+  sudo('chmod 2750 /sites/%s/index.php' % login)
   sudo('ln -s /sites/%s /assos/%s/public_html' % (login, login))
 
 def change_passwd(login, mdp):
